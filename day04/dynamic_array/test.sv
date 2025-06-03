5bf0a2639b73f163427a8557545f484d0a56de5b
Munees-Sanid <shanidshaan192@gmail.com>
2025-06-03 17:36:57 +0530

    update

--- day04/dynamic_array/test.sv
+++ day04/dynamic_array/test.sv
@@ -1,12 +1,15 @@
 module dyn_array_methods;
 
   int arr[] = '{6, 2, 9, 4, 7};
+  int min[$],max[$];
 
   initial begin
     $display("Sum     = %0d", arr.sum());
     $display("Product = %0d", arr.product());
-    $display("Min     = %0d", arr.min());
-    $display("Max     = %0d", arr.max());
+    min=arr.min();
+    $display("Min     = %0p", min);
+    max=arr.max();
+    $display("Max     = %0p", max);
   end
 
 endmodule
